96f5be6deb62359b184141b4962919c929272f88
natshathraaa <natshathra04@gmail.com>
2026-07-23 10:00:00 +0530

    Project scaffold: Vite + React base

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import FloatingWhatsApp from "./components/FloatingWhatsApp";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* Additional routes can be added here as the academy's content grows */}
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </main>
+        <Footer />
+        <FloatingWhatsApp />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
